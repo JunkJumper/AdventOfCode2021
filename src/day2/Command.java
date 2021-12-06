7a896d1fc917248416fe18b1acc3809659297ec2
Pauline <pauline.srifi@gmail.com>
2021-12-06 11:16:46 +0100

    up to day3

---
+++ src/day2/Command.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class Command {
+	protected String direction;
+	protected int lenght;
+	
+	public Command(String d, int l) {
+		this.direction = d;
+		this.lenght = l;
+	}
+}
